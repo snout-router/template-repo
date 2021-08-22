ad5828724d698fe9207e1f0b34a8d1844780c7eb
Erin Millard <ezzatron@gmail.com>
2021-08-22 11:53:46 +1000

    Explicitly specify primary API entry

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,6 +1,7 @@
 const {createConfig} = require('@snout/docusaurus-config')
 
 module.exports = createConfig({
+  primaryApiEntry: 'REPLACE_ME',
   rootPath: __dirname,
   title: 'Snout REPLACE_ME',
 })
