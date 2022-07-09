f70cf355f560a2b7ba36728db307d25bcd1ae13b
Erin Millard <contact@erin.id.au>
2022-07-09 23:22:14 +1000

    Convert website to TypeScript

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,33 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import React from 'react'
+import Feature from '../components/Feature'
+import Features from '../components/Features'
+import Hero from '../components/Hero'
+
+export default function Home () {
+  const context = useDocusaurusContext()
+  const {siteConfig: {tagline} = {}} = context
+
+  return (
+    <Layout description={tagline}>
+      <Hero />
+
+      <main>
+        <Features>
+          <Feature title='Tailored for purpose'>REPLACE_ME</Feature>
+
+          <Feature title='Teeny tiny'>
+            From day one, all <strong>Snout</strong> libraries have been written
+            with a focus on minimizing bundle size impact.
+          </Feature>
+
+          <Feature title='No baggage'>
+            <strong>Snout</strong> libraries don&apos;t attempt to directly
+            support legacy browsers, which keeps things maintainable.
+          </Feature>
+        </Features>
+      </main>
+    </Layout>
+  )
+}
